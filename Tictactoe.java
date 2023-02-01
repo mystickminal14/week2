fc8099c93449018e38d4e66b2bd1082773ee66d1
MINAL <121595966+mystickminal14@users.noreply.github.com>
2023-02-01 16:26:47 +0545

    Add files via upload

---
+++ Tictactoe.java
@@ -0,0 +1,13 @@
+public class Tictactoe{
+public static void main(String [] args){
+ 
+String ab="+--+--+--+";
+String bc="|  |  |  |";
+System.out.println(ab);
+System.out.println(bc);
+System.out.println(ab);
+System.out.println(bc);
+System.out.println(ab);
+System.out.println(bc);
+}}
+
