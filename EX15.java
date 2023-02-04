99adb588c022674d1b0f203c8939dcf46c0b454b
MINAL <121595966+mystickminal14@users.noreply.github.com>
2023-02-05 00:01:48 +0545

    Add files via upload

---
+++ EX15.java
@@ -0,0 +1,34 @@
+package assigment;
+/*final string LETTER_H = &quot;* *\n* *\n*****\n*
+*\n* *\n&quot;;
+(The \n escape sequence denotes a “newline” character that causes
+subsequent characters to be printed on a new line.)
+Do the same for the letters E, L, and O. Then write the message:
+H
+E
+L
+
+CIS016-1 Principles of Programming
+
+Page 5 of 10
+
+L
+O
+in large letters.*/
+public class EX15 {
+	public static void main(String []args) {
+final String LETTER_H="*   *\n*   *\n*   *\n*****\n*   *\n*   *\n*   *  ";
+final String LETTER_E="*****\n*   \n*    \n*****\n*   \n*    \n*****";
+final String LETTER_0=" ** \n*  *\n*  *\n*  *\n*  *\n*  *\n ** ";
+final String LETTER_L="*\n*\n*\n*\n*\n*\n*****";
+
+
+System.out.println(LETTER_H);
+System.out.println(LETTER_E);
+System.out.println(LETTER_L);
+System.out.println(LETTER_L);
+System.out.println(LETTER_0);
+
+	}
+
+}
